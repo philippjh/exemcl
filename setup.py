091b3d8e069ed18b69db8b5bd96e8b5f22443726
Philipp-Jan Honysz <philipp.honysz@udo.edu>
2021-01-08 18:47:24 +0100

    setup.py now defaults to Release mode compilation.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class CMakeBuild(build_ext):
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        self.debug = True
+        self.debug = False
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
